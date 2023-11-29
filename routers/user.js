7bdf2f6642cc69e3cdbd89783bcb6cb2cbf967ad
“TatyanaA” <t.alchikova@gmail.com>
2023-11-29 12:25:46 +0000

    add PATCH for Users

--- routers/user.js
+++ routers/user.js
@@ -9,5 +9,6 @@ userRouter.post("/login", userController.login);
 userRouter.get("/", userController.index);
 userRouter.get("/:id", userController.show);
 userRouter.delete("/:id", userController.destroy);
+userRouter.patch("/:id",userController.update);
 
 module.exports = userRouter;
