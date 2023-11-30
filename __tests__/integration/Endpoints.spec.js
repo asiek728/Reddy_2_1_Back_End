c295119b097d3bf763de302c19116a9580f8556d
gajani-yoga <jeyagajani@hotmail.co.uk>
2023-11-30 13:06:08 +0000

    modify endpoints test

--- __tests__/integration/Endpoints.spec.js
+++ __tests__/integration/Endpoints.spec.js
@@ -1,5 +1,7 @@
 const request = require('supertest')
 const app = require('../../app')
+const postRouter = require('../../routers/post')
+const postTask = require('../../routers/task')
 
 
 describe('api server', () => {
@@ -18,6 +20,22 @@ describe('api server', () => {
 
     test('responds to GET / with status 200', (done) => {
         request(api).get('/').expect(200, done)
+    })
+
+     test('responds to POST /goats with a 201 status code', (done) => {
+      const testData = {
+        title: "Clean beach",
+        date: 11/11/2023,
+        image_source: 'https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png',
+        content: 'We need to clean the beaches'
+      }
+  
+      request(api)
+        .post('/posts')
+        .send(testData)
+        .set('Accept', 'application/json')
+        .expect(201)
+        .expect({ data: { ...testData, id: 7 } }, done)
     })
 
 
